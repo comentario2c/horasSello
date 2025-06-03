beaedec01d3a1b1bc6b208b029c89a492b6de6e8
714 <misael.mezan7@gmail.com>
2025-06-03 12:04:22 -0400

    Inicio login admins

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LoginAdmComponent } from './login-adm/login-adm.component'; // ¡Importación correcta!
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginAdmComponent }, // Ruta para el login
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige la raíz a /login
+  { path: '**', redirectTo: '/login' } // Opcional: Redirige cualquier ruta no encontrada
+];
