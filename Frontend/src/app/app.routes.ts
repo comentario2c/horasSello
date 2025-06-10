b6c09632f0a2c031791bfcabbf1e317cea68c035
Pepeyon <lucas4molina@gmail.com>
2025-06-10 12:08:42 -0400

    commit con cambios de estructura y nuevos componentes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginAdmComponent } from './login-adm/login-adm.component'; // ¡Importación correcta!
+import { AlumnoComponent } from './alumno/alumno.component';
+import { HorasSellosComponent } from './horas-sellos/horas-sellos.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginAdmComponent }, // Ruta para el login
+  { path: 'alumno', component: AlumnoComponent },
+  { path: 'horas-sellos', component: HorasSellosComponent },
   { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige la raíz a /login
   { path: '**', redirectTo: '/login' } // Opcional: Redirige cualquier ruta no encontrada
 ];
